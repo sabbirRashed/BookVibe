523b6b3496268a07dceead8f81be1e4798e492bf
sabbirRashed <srsabbirrahman12@gmail.com>
2026-04-15 11:28:09 +0600

    displayed read list and wish list card

--- src/components/ListedReadList/ListedReadList.jsx
+++ src/components/ListedReadList/ListedReadList.jsx
@@ -4,7 +4,14 @@ import BookCard from '../ui/BookCard';
 import ListedBookCard from '../ui/ListedBookCard';
 
 const ListedReadList = () => {
-    const { markedBooks, wishlist } = useContext(BookContext);
+    const { markedBooks } = useContext(BookContext);
+
+    if (markedBooks.length === 0) {
+        return <div className='bg-base-300 py-20 rounded-2xl mt-8'>
+                <h2 className='text-center text-2xl md:text-3xl font-bold'>No read list data found</h2>
+        </div>
+    }
+
     return (
         <div className='mt-8 space-y-6'>
             {markedBooks.map((book, ind) => <ListedBookCard key={ind} book={book}></ListedBookCard>)}
--- src/components/ListedWishList/ListedWishList.jsx
+++ src/components/ListedWishList/ListedWishList.jsx
@@ -1,12 +1,19 @@
 import React, { useContext } from 'react';
 import { BookContext } from '../../Context/BookProvider';
+import ListedBookCard from '../ui/ListedBookCard';
 
 const ListedWishList = () => {
-     const { markedBooks, wishlist } = useContext(BookContext);
+    const { markedBooks, wishlist } = useContext(BookContext);
+
+    if (markedBooks.length === 0) {
+        return <div className='bg-base-300 py-20 rounded-2xl mt-8'>
+            <h2 className='text-center text-2xl md:text-3xl font-bold'>No wish list data found</h2>
+        </div>
+    }
 
     return (
-        <div className='border border-green-500 mt-8 space-y-6'>
-            <p>Listted wish list</p>
+        <div className='mt-8 space-y-6'>
+            {wishlist.map((book, ind) => <ListedBookCard key={ind} book={book}></ListedBookCard>)}
         </div>
     );
 };
